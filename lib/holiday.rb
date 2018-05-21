6ba4bd1bf1a90c447a8acb62341575537f5d48b6
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-21 23:24:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, item|
       if item.include?("BBQ")
-        return holiday
+        print holiday
       end
     end 
   end
